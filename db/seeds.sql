0854805dc75878c92ee33c4041f3f9b28c9209db
Lihuor <lihuor@gmail.com>
2020-08-06 19:55:08 +1000

    Fixed Boolean MySQL

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,6 +3,6 @@ USE burgers_db;
 INSERT INTO burgers 
 (burger_name, devoured) 
 VALUES 
-('Big Mac', 1),
-('Mac Chicken', 0),
-('Double big Mac', 1);
+('Big Mac',true),
+('Mac Chicken',false),
+('Double big Mac',true);
